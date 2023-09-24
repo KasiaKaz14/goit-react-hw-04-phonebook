ad12ebfccdddb9a39fd36e473aa0e38009c91175
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-09-24 14:32:48 +0200

    e

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,32 +1,7 @@
 import { useState } from 'react';
 import css from './Form.module.css';
 
-export const Form = ({ onSubmit }) => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-
-  const handleChange = event => {
-    const { name, number } = event.target;
-    switch (name) {
-      case 'name':
-        setName(name);
-        break;
-
-      case 'number':
-        setNumber(number);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    onSubmit(name, number);
-    setName('');
-    setNumber('');
-  };
-
+export const Form = ({ onSubmit, onChange }) => {
   return (
     <form onSubmit={onSubmit}>
       <label className={css.label}>
@@ -34,7 +9,7 @@ export const Form = ({ onSubmit }) => {
         <input
           type="text"
           name="name"
-          onChange={handleChange}
+          onChange={onChange}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
@@ -44,7 +19,7 @@ export const Form = ({ onSubmit }) => {
         <input
           type="tel"
           name="number"
-          onChange={handleChange}
+          onChange={onChange}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
