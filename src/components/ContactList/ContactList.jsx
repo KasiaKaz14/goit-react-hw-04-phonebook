92dbd4d7b70e1c8487d50f2ab5d6b7d6f9501bbd
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-09-27 14:10:44 +0200

    e

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,21 +1,11 @@
-import { useState } from 'react';
 import css from './ContactList.module.css';
 import PropTypes from 'prop-types';
 
 export const ContactList = ({ contacts, onClick }) => {
-  const [search, setSearch] = useState('');
-  const handleChange = e => {
-    setSearch(e.target.value);
-  };
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(search.toLowerCase())
-  );
-
   return (
     <div>
-      <input type="text" value={search} onChange={handleChange} />
       <ul className={css.list}>
-        {filteredContacts.map(contact => (
+        {contacts.map(contact => (
           <li className={css.listItem} key={contact.id}>
             {contact.name}: {contact.number}
             <button
